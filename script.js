6790cc03b035d253b50f0cdef0bc9d5563358231
MrLives-For-Today <imcalzada03@gmail.com>
2021-07-27 12:29:07 -0500

    Video Preview done!

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("page loaded...");
+// Plays video
+function playVid(element) {
+    element.play();
+}
+// Pauses video
+function pauseVid(element) {
+    element.pause();
+}
+// Mute the video
+document.getElementById(element).muted = true;
